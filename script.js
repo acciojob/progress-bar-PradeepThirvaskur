c0c226f55de07baa48e415663089b344c900f08d
pradeep thirvaskur <142670238+PradeepThirvaskur@users.noreply.github.com>
2024-10-21 13:05:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,46 @@
-const circles = document.querySelectorAll('.circle');
-const prevButton = document.getElementById('prev');
-const nextButton = document.getElementById('next');
+window.onload = function() {
+  const circles = document.querySelectorAll('.circle');
+  const prevButton = document.getElementById('prev');
+  const nextButton = document.getElementById('next');
 
-let currentActive = 1;
+  let currentActive = 1;
 
-nextButton.addEventListener('click', () => {
-  currentActive++;
+  nextButton.addEventListener('click', () => {
+    currentActive++;
 
-  if (currentActive > circles.length) {
-    currentActive = circles.length;
-  }
-
-  update();
-});
-
-prevButton.addEventListener('click', () => {
-  currentActive--;
-
-  if (currentActive < 1) {
-    currentActive = 1;
-  }
-
-  update();
-});
-
-function update() {
-  circles.forEach((circle, idx) => {
-    if (idx < currentActive) {
-      circle.classList.add('active');
-    } else {
-      circle.classList.remove('active');
+    if (currentActive > circles.length) {
+      currentActive = circles.length;
     }
+
+    update();
   });
 
-  if (currentActive === 1) {
-    prevButton.disabled = true;
-  } else if (currentActive === circles.length) {
-    nextButton.disabled = true;
-  } else {
-    prevButton.disabled = false;
-    nextButton.disabled = false;
+  prevButton.addEventListener('click', () => {
+    currentActive--;
+
+    if (currentActive < 1) {
+      currentActive = 1;
+    }
+
+    update();
+  });
+
+  function update() {
+    circles.forEach((circle, idx) => {
+      if (idx < currentActive) {
+        circle.classList.add('active');
+      } else {
+        circle.classList.remove('active');
+      }
+    });
+
+    if (currentActive === 1) {
+      prevButton.disabled = true;
+    } else if (currentActive === circles.length) {
+      nextButton.disabled = true;
+    } else {
+      prevButton.disabled = false;
+      nextButton.disabled = false;
+    }
   }
 }
